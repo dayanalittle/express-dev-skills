657017b9fccdcf5a7cb9557eb415ffeeb824c184
Day Little <dayana.y.little@gmail.com>
2023-01-07 00:59:10 -0500

    add data

---
+++ data/skills-data.js
@@ -0,0 +1,9 @@
+const skill = [
+    { text: 'Write', Can Do: false, _id: 125223 },
+    { text: 'Read', Can Do : false, _id: 127904 },
+    { text: 'Text', Can Do: true, _id: 139608 },
+]
+
+export {
+    inventory
+}
